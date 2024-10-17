dfe0dc3266845422ac2e1e3419b7f44c0d3d9d19
“MANPREET <gulatimk.ca@gmail.com>
2024-10-17 15:54:26 -0400

    Initial Commit

---
+++ CashFloatTests/CashFloatTests.swift
@@ -0,0 +1,17 @@
+//
+//  CashFloatTests.swift
+//  CashFloatTests
+//
+//  Created by Manpreet Gulati on 17/10/24.
+//
+
+import Testing
+@testable import CashFloat
+
+struct CashFloatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
